f83d46ed129a7a0a2184906b8ddd0aeb1ecf1539
Zayne Abraham <zayneabraham1@gmail.com>
2017-02-05 23:13:21 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,41 +2,26 @@ require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_supplies[:summer][:fourth_of_july][1]
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter][:christmas] = ""
-  holiday_hash[:winter][:christmas] = "Lights", "Wreath", supply
-  holiday_hash[:winter][:new_years] = ""
-  holiday_hash[:winter][:new_years] = "Party Hats", supply
+  holiday_hash[:winter].each do |k, v|
+    v << supply
+  end
+  holiday_hash[:spring].each do |k, v|
+    v << supply
+  end
 end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash[:spring][:memorial_day] = ""
-  holiday_hash[:spring][:memorial_day] = "BBQ", supply
+  holiday_hash[:spring].each do |k,v|
+    v << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
@@ -79,3 +64,21 @@ def all_holidays_with_bbq(holiday_hash)
     end
     array
  end
+
+ # given that holiday_hash looks like this:
+ # {
+ #   :winter => {
+ #     :christmas => ["Lights", "Wreath"],
+ #     :new_years => ["Party Hats"]
+ #   },
+ #   :summer => {
+ #     :fourth_of_july => ["Fireworks", "BBQ"]
+ #   },
+ #   :fall => {
+ #     :thanksgiving => ["Turkey"]
+ #   },
+ #   :spring => {
+ #     :memorial_day => ["BBQ"]
+ #   }
+ # }
+ # return the second element in the 4th of July array
